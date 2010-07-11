42036cd10b4a37c0e6874311de5bfb6bd77a8453
Olaf Bergmann <bergmann@tzi.org>
2010-07-11 19:58:32 +0200

    remove client functionality (see examples/client.c)

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /* coap -- simple implementation of the Constrained Application Protocol (CoAP)
- *         as defined in draft-ietf-core-coap-00
+ *         as defined in draft-ietf-core-coap-01
  *
  * (c) 2010 Olaf Bergmann <bergmann@tzi.org>
  */
@@ -19,115 +19,86 @@
 #include "coap.h"
 
 coap_pdu_t *
-coap_new_get( const char *uri ) {
-  coap_pdu_t *pdu;
-
-  if ( ! ( pdu = coap_new_pdu() ) )
-    return NULL;
-
-  pdu->hdr->type = COAP_MESSAGE_CON;
-  pdu->hdr->code = COAP_REQUEST_GET;
-
-  if ( uri && *uri ) {
-    if ( *uri != '/' )
-      coap_add_option ( pdu, COAP_OPTION_URI_FULL, strlen( uri ), (unsigned char *)uri );
-    else {
-      ++uri;
-      if ( *uri )
-	coap_add_option ( pdu, COAP_OPTION_URI_PATH, strlen( uri ), (unsigned char *)uri );
-    }
+new_ack( coap_context_t  *ctx, coap_queue_t *node ) {
+  coap_pdu_t *pdu = coap_new_pdu();
+  
+  if (pdu) {
+    pdu->hdr->type = COAP_MESSAGE_ACK;
+    pdu->hdr->code = 0;
+    pdu->hdr->id = node->pdu->hdr->id;
   }
 
   return pdu;
 }
 
-void 
-send_request( coap_context_t  *ctx, coap_pdu_t  *pdu, const char *server, unsigned short port ) {
-  struct addrinfo *res, *ainfo;
-  struct addrinfo hints;
-  int error;
-  struct sockaddr_in6 dst;
+coap_pdu_t *
+new_response( coap_context_t  *ctx, coap_queue_t *node, unsigned int code ) {
+  coap_pdu_t *pdu = new_ack(ctx, node);
 
-  memset ((char *)&hints, 0, sizeof(hints));
-  hints.ai_socktype = SOCK_DGRAM;
-  hints.ai_family = AF_INET6;
+  if (pdu)
+    pdu->hdr->code = code;
 
-  error = getaddrinfo(server, "", &hints, &res);
-
-  if (error != 0) {
-    perror("getaddrinfo");
-    exit(1);
-  }
-
-
-  for (ainfo = res; ainfo != NULL; ainfo = ainfo->ai_next) {
-
-    if ( ainfo->ai_family == AF_INET6 ) {
-
-      memset(&dst, 0, sizeof dst );
-      dst.sin6_family = AF_INET6;
-      dst.sin6_port = htons( port );
-      memcpy( &dst.sin6_addr, &((struct sockaddr_in6 *)ainfo->ai_addr)->sin6_addr, sizeof(dst.sin6_addr) );
-
-      coap_send_confirmed( ctx, &dst, pdu );
-      goto leave;
-    }
-  }
- 
- leave:
-  freeaddrinfo(res);
+  return pdu;
 }
+
+void
+add_contents( coap_pdu_t *pdu, unsigned int mediatype, unsigned int len, unsigned char *data ) {
+  unsigned char ct = COAP_MEDIATYPE_APPLICATION_LINK_FORMAT;
+  if (!pdu)
+    return;
+  
+  /* add content-encoding */
+  coap_add_option(pdu, COAP_OPTION_CONTENT_TYPE, 1, &ct);
+
+  /* TODO: handle fragmentation (check result code) */
+  coap_add_data(pdu, len, data);
+}
+
+static unsigned char resources[] = 
+  "</first-resource>;sh=/f;n=Example Resource 1,"
+  "</second-resource>;sh=/s;ct=41,42;n=second example";
 
 void 
 message_handler( coap_context_t  *ctx, coap_queue_t *node, void *data) {
+  coap_pdu_t *pdu = NULL;
+
 #ifndef NDEBUG
   printf("** process pdu: ");
   coap_show_pdu( node->pdu );
 #endif
-}
 
-void
-split_uri( char *str, char **server, unsigned short *port, char **path ) {
-  char *p;
-  *port = COAP_DEFAULT_PORT;
-  
-  if ( strncmp( str, "coap://", 7 ) == 0 ) {
-    *server = str + 7;
-    
-    *path = *server;
-    /* note that we do not support URIs like coap://server?query or coap://server#fragment */
-    while ( **path && **path != '/' ) 
-      ++*path;
-
-    if ( **path ) {
-      **path = '\0';
-      ++*path;
-    }
-
-    /* split server address and port */
-    if ( **server == '[' ) {	/* IPv6 address reference */
-      p = ++*server;
-      while ( *p && *p != ']' ) 
-	++p;
-      *p++ = '\0';
-    } else {			/* IPv4 address or hostname */
-      p = *server;
-      while ( *p && *p != ':' ) 
-	++p;
-    }
-    
-    if ( *p++ == ':' ) {	/* handle port */
-      *port = 0;
-      while ( isdigit(*p) ) {
-	*port *= 10;
-	*port += *p - '0';
-	++p;
-      }
-    }
-
-  } else {
-    *path = str;
+  if ( node->pdu->hdr->version != COAP_DEFAULT_VERSION ) {
+    debug("dropped packet with unknown version %u\n", node->pdu->hdr->version);
+    return;
   }
+    
+  switch (node->pdu->hdr->code) {
+  case COAP_REQUEST_GET :
+    pdu = new_response( ctx, node, COAP_RESPONSE_200 );
+    if (pdu) {
+      add_contents( pdu, COAP_MEDIATYPE_APPLICATION_LINK_FORMAT, 
+		    (unsigned int)strlen((char *)resources), resources );
+    }
+    break;
+  case COAP_REQUEST_POST:
+  case COAP_REQUEST_PUT:
+  case COAP_REQUEST_DELETE:
+    debug("request method not implemented: %u\n", node->pdu->hdr->code);
+
+    /* TODO: send 165 response */
+    pdu = new_response( ctx, node, COAP_RESPONSE_405 );
+    break;
+  default:
+      if ( node->pdu->hdr->code >= COAP_RESPONSE_100 && node->pdu->hdr->type == COAP_MESSAGE_CON ) {
+	pdu = new_ack( ctx, node );
+      }
+  }
+
+  if ( pdu && coap_send( ctx, &node->remote, pdu ) == COAP_INVALID_TID ) {
+    debug("message_handler: error sending reponse");
+    coap_delete_pdu(pdu);
+  }
+
 }
 
 void 
@@ -213,8 +184,6 @@ main(int argc, char **argv) {
   int result;
   time_t now;
   coap_queue_t *nextpdu;
-  coap_pdu_t  *pdu;
-  static char *server = NULL, *path = NULL;
   unsigned short port = COAP_DEFAULT_PORT;
   int opt;
   char *group = NULL;
@@ -237,22 +206,10 @@ main(int argc, char **argv) {
   if ( !ctx )
     return -1;
 
-  coap_register_message_handler( ctx, message_handler );
-
-  if ( optind < argc )
-    split_uri( argv[optind], &server, &port, &path );
-  else {
-    usage( argv[0], VERSION );
-    exit( 1 );
-  }
-
   if ( group )
     join( ctx, group );
 
-  if (! (pdu = coap_new_get( path ) ) )
-    return -1;
-
-  send_request( ctx, pdu, server ? server : "::1", port );
+  coap_register_message_handler( ctx, message_handler );
 
   while ( 1 ) {
     FD_ZERO(&readfds); 
