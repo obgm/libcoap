019a922918e99e218deec83a14fb84223e9aad68
Olaf Bergmann <bergmann@tzi.org>
2013-01-07 17:48:09 +0100

    document known bugs

--- tests/testdriver.c
+++ tests/testdriver.c
@@ -25,5 +25,16 @@ main(int argc, char **argv) {
   result = CU_basic_run_tests();
 
   CU_cleanup_registry();
+
+  printf("\n\nknown bugs:\n");
+  printf("\t- Test: t_parse_uri5 ... FAILED\n"
+	 "\t    1. test_uri.c:109  - CU_FAIL(\"invalid port not detected\")\n");
+  printf("\t- Test: t_parse_uri12 ... FAILED\n"
+    	 "\t    1. test_uri.c:301  - result == 4\n"
+    	 "\t    2. test_uri.c:302  - buflen == sizeof(uricheckbuf)\n"
+    	 "\t    3. test_uri.c:303  - CU_ASSERT_NSTRING_EQUAL(buf,uricheckbuf,buflen)\n"
+         "\t    4. test_uri.c:309  - buflen == sizeof(querycheckbuf)\n"
+         "\t    5. test_uri.c:310  - CU_ASSERT_NSTRING_EQUAL(buf,querycheckbuf,buflen)\n");
+
   return result;
 }
