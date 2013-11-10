652bdd5f63e5bc48f3f6f33de9576167c42d5192
Olaf Bergmann <bergmann@tzi.org>
2013-11-10 11:52:19 +0100

    resolved conflict after cherry-picking from branch relative-timestamps

--- tests/testdriver.c
+++ tests/testdriver.c
@@ -9,6 +9,7 @@
 #include "test_options.h"
 #include "test_pdu.h"
 #include "test_error_response.h"
+#include "test_sendqueue.h"
 #include "test_wellknown.h"
 
 
@@ -26,6 +27,7 @@ main(int argc, char **argv) {
   t_init_option_tests();
   t_init_pdu_tests();
   t_init_error_response_tests();
+  t_init_sendqueue_tests();
   t_init_wellknown_tests();
 
   CU_basic_set_mode(run_mode);
