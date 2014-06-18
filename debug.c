143597a71d1ab0c6a2a4823d237f65ef2da8936a
Olaf Bergmann <bergmann@tzi.org>
2014-06-19 00:11:16 +0200

    Improved logging function for Contiki.
    Use vprintf() for log output when HAVE_VPRINTF is defined. Otherwise,
    PRINTF will be used which does not work together with va_args.

--- debug.c
+++ debug.c
@@ -237,6 +237,12 @@ coap_print_addr(const struct coap_address_t *addr, unsigned char *buf, size_t le
 #ifdef WITH_CONTIKI
 # define fprintf(fd, ...) PRINTF(__VA_ARGS__)
 # define fflush(...)
+
+# ifdef HAVE_VPRINTF
+#  define vfprintf(fd, ...) vprintf(__VA_ARGS__)
+# else /* HAVE_VPRINTF */
+#  define vfprintf(fd, ...) PRINTF(__VA_ARGS__)
+# endif /* HAVE_VPRINTF */
 #endif /* WITH_CONTIKI */
 
 void
@@ -297,7 +303,6 @@ coap_show_pdu(const coap_pdu_t *pdu) {
 
 #endif /* NDEBUG */
 
-#ifndef WITH_CONTIKI
 void 
 coap_log_impl(coap_log_t level, const char *format, ...) {
   char timebuf[32];
@@ -322,25 +327,4 @@ coap_log_impl(coap_log_t level, const char *format, ...) {
   va_end(ap);
   fflush(log_fd);
 }
-#else /* WITH_CONTIKI */
-void 
-coap_log_impl(coap_log_t level, const char *format, ...) {
-  char timebuf[32];
-  coap_tick_t now;
-  va_list ap;
 
-  if (maxlog < level)
-    return;
-  
-  coap_ticks(&now);
-  if (print_timestamp(timebuf,sizeof(timebuf), now))
-    PRINTF("%s ", timebuf);
-
-  if (level <= LOG_DEBUG)
-    PRINTF("%s ", loglevels[level]);
-
-  va_start(ap, format);
-  PRINTF(format, ap);
-  va_end(ap);
-}
-#endif /* WITH_CONTIKI */
