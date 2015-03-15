d15ecb60ec17e0f9b0825d4f6705f6e4264232d2
Carsten Schoenert <c.schoenert@t-online.de>
2015-03-15 13:00:44 +0100

    i/c/prng.h: whitespace cleanup, reformatting, no changes
    Just some cleanup like whitespace, punctiation marking and indentation
    issues.

--- include/coap/prng.h
+++ include/coap/prng.h
@@ -3,10 +3,10 @@
  * Copyright (C) 2010,2011 Olaf Bergmann <bergmann@tzi.org>
  *
  * This file is part of the CoAP library libcoap. Please see
- * README for terms of use. 
+ * README for terms of use.
  */
 
-/** 
+/**
  * @file prng.h
  * @brief Pseudo Random Numbers
  */
@@ -16,7 +16,7 @@
 
 #include "coap_config.h"
 
-/** 
+/**
  * @defgroup prng Pseudo Random Numbers
  * @{
  */
@@ -25,9 +25,9 @@
 #include <stdlib.h>
 
 /**
- * Fills \p buf with \p len random bytes. This is the default
- * implementation for prng().  You might want to change prng() to use
- * a better PRNG on your specific platform.
+ * Fills \p buf with \p len random bytes. This is the default implementation for
+ * prng(). You might want to change prng() to use a better PRNG on your specific
+ * platform.
  */
 static inline int
 coap_prng_impl(unsigned char *buf, size_t len) {
@@ -39,9 +39,9 @@ coap_prng_impl(unsigned char *buf, size_t len) {
 #include <string.h>
 
 /**
- * Fills \p buf with \p len random bytes. This is the default
- * implementation for prng().  You might want to change prng() to use
- * a better PRNG on your specific platform.
+ * Fills \p buf with \p len random bytes. This is the default implementation for
+ * prng(). You might want to change prng() to use a better PRNG on your specific
+ * platform.
  */
 static inline int
 contiki_prng_impl(unsigned char *buf, size_t len) {
@@ -62,18 +62,18 @@ contiki_prng_impl(unsigned char *buf, size_t len) {
 #endif /* WITH_CONTIKI */
 
 #ifndef prng
-/** 
- * Fills \p Buf with \p Length bytes of random data. 
- * 
+/**
+ * Fills \p Buf with \p Length bytes of random data.
+ *
  * @hideinitializer
  */
 #define prng(Buf,Length) coap_prng_impl((Buf), (Length))
 #endif
 
 #ifndef prng_init
-/** 
- * Called to set the PRNG seed. You may want to re-define this to
- * allow for a better PRNG.
+/**
+ * Called to set the PRNG seed. You may want to re-define this to allow for a
+ * better PRNG.
  *
  * @hideinitializer
  */
