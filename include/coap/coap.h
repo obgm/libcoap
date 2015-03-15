9da72684c8904b11b15b32bb3831456ada7e07f1
Carsten Schoenert <c.schoenert@t-online.de>
2015-03-15 12:57:17 +0100

    i/c/coap.h: whitespace cleanup, reformatting, no changes
    Just some whitespace cleanup.

--- include/coap/coap.h
+++ include/coap/coap.h
@@ -3,7 +3,7 @@
  * Copyright (C) 2010--2012,2015 Olaf Bergmann <bergmann@tzi.org>
  *
  * This file is part of the CoAP library libcoap. Please see
- * README for terms of use. 
+ * README for terms of use.
  */
 
 #ifndef _COAP_H_
