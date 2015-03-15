e2683eb2186e90b80cac991ba6e0f93d6d82f46a
Carsten Schoenert <c.schoenert@t-online.de>
2015-03-15 12:57:15 +0100

    i/c/bits.h: whitespace cleanup, reformating, no changes
    Just some cleanup like whitespace, punctiation marking and indentation
    issues.

--- include/coap/bits.h
+++ include/coap/bits.h
@@ -4,12 +4,12 @@
  * Copyright (C) 2010,2011 Olaf Bergmann <bergmann@tzi.org>
  *
  * This file is part of the CoAP library libcoap. Please see
- * README for terms of use. 
+ * README for terms of use.
  */
 
 /**
  * @file bits.h
- * @brief bit vector manipulation
+ * @brief Bit vector manipulation
  */
 
 #ifndef _BITS_H_
@@ -18,16 +18,16 @@
 #include <sys/types.h>
 #include <stdint.h>
 
-/** 
- * Sets the bit @p bit in bit-vector @p vec. This function returns @c
- * 1 if bit was set or @c -1 on error (i.e. when the given bit does
- * not fit in the vector).
- * 
+/**
+ * Sets the bit @p bit in bit-vector @p vec. This function returns @c 1 if bit
+ * was set or @c -1 on error (i.e. when the given bit does not fit in the
+ * vector).
+ *
  * @param vec  The bit-vector to change.
  * @param size The size of @p vec in bytes.
  * @param bit  The bit to set in @p vec.
- * 
- * @return @c -1 if @p bit does not fit into @p vec, @c 1 otherwise.
+ *
+ * @return     @c -1 if @p bit does not fit into @p vec, @c 1 otherwise.
  */
 inline static int
 bits_setb(uint8_t *vec, size_t size, uint8_t bit) {
@@ -38,16 +38,16 @@ bits_setb(uint8_t *vec, size_t size, uint8_t bit) {
   return 1;
 }
 
-/** 
- * Clears the bit @p bit from bit-vector @p vec. This function returns
- * @c 1 if bit was cleared or @c -1 on error (i.e. when the given bit
- * does not fit in the vector).
- * 
+/**
+ * Clears the bit @p bit from bit-vector @p vec. This function returns @c 1 if
+ * bit was cleared or @c -1 on error (i.e. when the given bit does not fit in
+ * the vector).
+ *
  * @param vec  The bit-vector to change.
  * @param size The size of @p vec in bytes.
  * @param bit  The bit to clear from @p vec.
- * 
- * @return @c -1 if @p bit does not fit into @p vec, @c 1 otherwise.
+ *
+ * @return     @c -1 if @p bit does not fit into @p vec, @c 1 otherwise.
  */
 inline static int
 bits_clrb(uint8_t *vec, size_t size, uint8_t bit) {
@@ -58,15 +58,15 @@ bits_clrb(uint8_t *vec, size_t size, uint8_t bit) {
   return 1;
 }
 
-/** 
+/**
  * Gets the status of bit @p bit from bit-vector @p vec. This function returns
- * @c 1 if the bit is set, @c 0 otherwise (even in case of an error)..
- * 
- * @param vec  The bit-vector to read from..
+ * @c 1 if the bit is set, @c 0 otherwise (even in case of an error).
+ *
+ * @param vec  The bit-vector to read from.
  * @param size The size of @p vec in bytes.
  * @param bit  The bit to get from @p vec.
- * 
- * @return @c 1 if the bit is set, @c 0 otherwise.
+ *
+ * @return     @c 1 if the bit is set, @c 0 otherwise.
  */
 inline static int
 bits_getb(const uint8_t *vec, size_t size, uint8_t bit) {
