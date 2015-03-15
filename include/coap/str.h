1d79a041ba98eabfef91b4b8cde59d4ab2cf1411
Carsten Schoenert <c.schoenert@t-online.de>
2015-03-15 13:00:46 +0100

    i/c/str.h: whitespace cleanup, reformatting, no changes
    Just some cleanup like whitespace, punctiation marking and indentation
    issues.

--- include/coap/str.h
+++ include/coap/str.h
@@ -3,7 +3,7 @@
  * Copyright (C) 2010,2011 Olaf Bergmann <bergmann@tzi.org>
  *
  * This file is part of the CoAP library libcoap. Please see
- * README for terms of use. 
+ * README for terms of use.
  */
 
 #ifndef _COAP_STR_H_
@@ -12,19 +12,21 @@
 #include <string.h>
 
 typedef struct {
-  size_t length;		/* length of string */
-  unsigned char *s;		/* string data */
+  size_t length;    /* length of string */
+  unsigned char *s; /* string data */
 } str;
 
 #define COAP_SET_STR(st,l,v) { (st)->length = (l), (st)->s = (v); }
 
 /**
- * Returns a new string object with at least size bytes storage
- * allocated.  The string must be released using coap_delete_string();
+ * Returns a new string object with at least size bytes storage allocated. The
+ * string must be released using coap_delete_string();
  */
 str *coap_new_string(size_t size);
 
-/** Deletes the given string and releases any memory allocated. */
+/**
+ * Deletes the given string and releases any memory allocated.
+ */
 void coap_delete_string(str *);
 
 #endif /* _COAP_STR_H_ */
