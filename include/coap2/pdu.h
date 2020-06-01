9dd16a32602dc6749278371c0a7e37b473b8ffb0
Jon Shallow <supjps-libcoap@jpshallow.com>
2020-06-01 11:29:03 +0100

    RFC7872: Add in media type application/dots+cbor
    include/coap2/pdu.h:
    src/coap_debug.c:
    
    Include in coap_show_pdu() output if appropriate.

--- include/coap2/pdu.h
+++ include/coap2/pdu.h
@@ -208,6 +208,9 @@ const char *coap_response_phrase(unsigned char code);
 #define COAP_MEDIATYPE_APPLICATION_SENML_XML    310 /* application/senml+xml   */
 #define COAP_MEDIATYPE_APPLICATION_SENSML_XML   311 /* application/sensml+xml  */
 
+/* Content formats from RFC 8782 */
+#define COAP_MEDIATYPE_APPLICATION_DOTS_CBOR    271 /* application/dots+cbor */
+
 /* Note that identifiers for registered media types are in the range 0-65535. We
  * use an unallocated type here and hope for the best. */
 #define COAP_MEDIATYPE_ANY                         0xff /* any media type */
