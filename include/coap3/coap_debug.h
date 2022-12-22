6599297edbfa13c0d0511a037e126c0dfc41aaa2
Jon Shallow <supjps-libcoap@jpshallow.com>
2022-12-22 12:50:10 +0000

    syslog.h: Remove all references to syslog.h
    syslog.h is no longer required for libcoap as all LOG_ usage has been replaced
    with COAP_LOG_.
    
    Added #include <stdio.h> to example code for code-client and coap-server as
    a hint should application code be using LOG_.

--- include/coap3/coap_debug.h
+++ include/coap3/coap_debug.h
@@ -67,44 +67,6 @@ typedef enum {
 #define COAP_LOG_CIPHERS COAP_LOG_DTLS_BASE /* For backward compatability */
 } coap_log_t;
 
-#ifdef HAVE_SYSLOG_H
-#include <syslog.h>
-#else
-#if defined(RIOT_VERSION)
-/* RIOT defines a subset of the syslog levels in log.h with different
- * numeric values. The remaining levels are defined here. Note that
- * output granularity differs from what would be expected when
- * adhering to the syslog levels.
- */
-#include <log.h>
-#endif /* RIOT_VERSION */
-#endif /* HAVE_SYSLOG_H */
-
-#ifndef LOG_EMERG
-# define LOG_EMERG  COAP_LOG_EMERG
-#endif
-#ifndef LOG_ALERT
-# define LOG_ALERT  COAP_LOG_ALERT
-#endif
-#ifndef LOG_CRIT
-# define LOG_CRIT   COAP_LOG_CRIT
-#endif
-#ifndef LOG_ERR
-# define LOG_ERR    COAP_LOG_ERR
-#endif
-#ifndef LOG_WARNING
-# define LOG_WARNING COAP_LOG_WARN
-#endif
-#ifndef LOG_NOTICE
-# define LOG_NOTICE COAP_LOG_NOTICE
-#endif
-#ifndef LOG_INFO
-# define LOG_INFO   COAP_LOG_INFO
-#endif
-#ifndef LOG_DEBUG
-# define LOG_DEBUG  COAP_LOG_DEBUG
-#endif
-
 /**
  * Get the current logging level.
  *
