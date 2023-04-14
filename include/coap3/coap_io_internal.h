7d3780a5413c3f92609b76f078b4b667bd520699
Jon Shallow <supjps-libcoap@jpshallow.com>
2023-04-14 10:45:02 +0100

    Layers: Move layer index table into coap_layers.c

--- include/coap3/coap_io_internal.h
+++ include/coap3/coap_io_internal.h
@@ -18,6 +18,7 @@
 #define COAP_IO_INTERNAL_H_
 
 #include "coap_internal.h"
+#include "coap_layers_internal.h"
 #include <sys/types.h>
 
 #include "coap_address.h"
@@ -29,99 +30,6 @@ struct uip_udp_conn;
 #ifdef RIOT_VERSION
 #include "net/gnrc.h"
 #endif /* RIOT_VERSION */
-
-typedef enum {
-  COAP_LAYER_SESSION,
-  COAP_LAYER_WS,
-  COAP_LAYER_TLS,
-  COAP_LAYER_LAST
-} coap_layer_t;
-
-/**
- * Function read interface for layer data receiving.
- *
- * If a called lower layer returned value is 0 or less, this must get passed
- * back to the caller.
- *
- * If the layer function consumes all the data (i.e. to handle the protocol
- * layer requirements), then the function must return 0 to indicate no data.
- *
- * Otherwise data must get updated (limited by datalen) and the number of bytes
- * available returned.
- *
- * Note: If the number of returned bytes is less that read in, then
- * COAP_SOCKET_CAN_READ must be dropped from session->sock.flags.
- *
- * @param session  Session to receive data on.
- * @param data     The data to receive.
- * @param datalen  The maximum length of @p data.
- *
- * @return         >=0 Number of bytes read.
- *                 -1  Error error in errno).
- *                 -2  Recieved ICMP unreachable.
- */
-typedef ssize_t (*coap_layer_read_t)(coap_session_t *session,
-                                      uint8_t *data, size_t datalen);
-
-/**
- * Function write interface for layer data sending.
- *
- * If a called lower layer returned value is 0 or less, this must get passed
- * back to the caller.
- *
- * If the layer function cannot transmit any data (congestion control etc.),
- * then the function must return 0 to indicate no data sent.
- *
- * It is possible that not all the data is sent (congestion control etc.),
- * and bytes written is less that datalen.
- *
- * Note: If the number of returned bytes is less that to be written,
- * COAP_SOCKET_WANT_WRITE must be added to session->sock.flags.
- *
- * @param session  Session to receive data on.
- * @param data     The data to write out.
- * @param datalen  The maximum length of @p data.
- *
- * @return         >=0 Number of bytes written.
- *                 -1  Error error in errno).
- */
-typedef ssize_t (*coap_layer_write_t)(coap_session_t *session,
-                                      const uint8_t *data, size_t datalen);
-/**
- * Function establish interface for layer establish handling.
- *
- * If this layer is properly established on invocation, then the next layer
- * must get called by calling
- *   session->lfunc[_this_layer_].establish(session)
- * (or done at any point when layer is established).
- * If the establishment of a layer fails, then
- *   coap_session_disconnected(session, COAP_NACK_xxx_LAYER_FAILED) must be
- *   called.
- *
- * @param session Session being established
- */
-typedef void (*coap_layer_establish_t)(coap_session_t *session);
-
-/**
- * Function close interface for layer closing.
- *
- * When this layer is properly closed, then the next layer
- * must get called by calling
- *   session->lfunc[_this_layer_].close(session)
- * (or done at any point when layer is closed).
- *
- * @param session Session being closed.
- */
-typedef void (*coap_layer_close_t)(coap_session_t *session);
-
-typedef struct {
-  coap_layer_read_t read;   /* Get data from next layer (TCP) */
-  coap_layer_write_t write; /* Output data to next layer */
-  coap_layer_establish_t establish; /* Layer establish */
-  coap_layer_close_t close; /* Connection close */
-} coap_layer_func_t;
-
-extern coap_layer_func_t coap_layers_coap[COAP_PROTO_LAST][COAP_LAYER_LAST];
 
 struct coap_socket_t {
 #if defined(WITH_LWIP)
---
+++ src/coap_layers.c
@@ -0,0 +1,111 @@
+/* coap_layers.c -- Layer handling for libcoap
+ *
+ * Copyright (C) 2023 Jon Shallow <supjps-libcoap@jpshallow.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * This file is part of the CoAP library libcoap. Please see
+ * README for terms of use.
+ */
+
+/**
+ * @file coap_layers.c
+ * @brief Layer Handling
+ */
+
+#include "coap3/coap_internal.h"
+
+/*
+ * Layer index table.  A whole protocol chunk gets copied into coap_socket_t.
+ * Each layer invokes the function defined at its layer to get the next layer
+ * (which could be above or below) to complete.
+ *
+ * The stack layers are (* managed by libcoap)
+ *   Application
+ *   CoAP *
+ *   CoAP-Session *
+ *   DTLS *
+ *   Netif *
+ *   Sockets *
+ *   Network Stack
+ *
+ * dgrm read currently handled separately.
+ * strm read works down the layers.
+ * write     works down the layers.
+ * establish is done after netif accept/connect completes by invoking SESSION
+ *           and then works up the layers.
+ * close     works down the layers
+ */
+coap_layer_func_t coap_layers_coap[COAP_PROTO_LAST][COAP_LAYER_LAST] = {
+  { /* COAP_PROTO_NONE */
+    { NULL, NULL, NULL, NULL }, /* SESSION */
+    { NULL, NULL, NULL, NULL }, /* WS */
+    { NULL, NULL, NULL, NULL }  /* TLS */
+  },
+  { /* COAP_PROTO_UDP */
+    { NULL,                 coap_netif_dgrm_write, coap_session_establish, coap_netif_close }, /* SESSION */
+    { NULL,                 NULL,                  NULL,                   NULL             }, /* WS */
+    { NULL,                 NULL,                  NULL,                   NULL             }  /* TLS */
+  },
+  { /* COAP_PROTO_DTLS */
+    { NULL,                 coap_dtls_send,        coap_dtls_establish,    coap_dtls_close  }, /* SESSION */
+    { NULL,                 NULL,                  NULL,                   NULL             }, /* WS */
+    { NULL,                 coap_netif_dgrm_write, coap_session_establish, coap_netif_close }  /* TLS */
+  },
+#if !COAP_DISABLE_TCP
+  { /* COAP_PROTO_TCP */
+    { coap_netif_strm_read, coap_netif_strm_write, coap_session_establish, coap_netif_close }, /* SESSION */
+    { NULL,                 NULL,                  NULL,                   NULL             }, /* WS */
+    { NULL,                 NULL,                  NULL,                   NULL             }  /* TLS */
+  },
+  { /* COAP_PROTO_TLS */
+    { coap_tls_read,        coap_tls_write,        coap_tls_establish,     coap_tls_close   }, /* SESSION */
+    { NULL,                 NULL,                  NULL,                   NULL             }, /* WS */
+    { coap_netif_strm_read, coap_netif_strm_write, coap_session_establish, coap_netif_close }  /* TLS */
+  },
+#if COAP_WS_SUPPORT
+  { /* COAP_PROTO_WS */
+    { coap_ws_read,         coap_ws_write,         coap_ws_establish,      coap_ws_close    }, /* SESSION */
+    { coap_netif_strm_read, coap_netif_strm_write, coap_session_establish, coap_netif_close }, /* WS */
+    { NULL,                 NULL,                  NULL,                   NULL             }  /* TLS */
+  },
+  { /* COAP_PROTO_WSS */
+    { coap_ws_read,         coap_ws_write,         coap_tls_establish,     coap_ws_close    }, /* SESSION */
+    { coap_tls_read,        coap_tls_write,        coap_session_establish, coap_tls_close   }, /* WS */
+    { coap_netif_strm_read, coap_netif_strm_write, coap_ws_establish,      coap_netif_close }  /* TLS */
+  }
+#else /* !COAP_WS_SUPPORT */
+  { /* COAP_PROTO_WS */
+    { NULL, NULL, NULL, NULL }, /* SESSION */
+    { NULL, NULL, NULL, NULL }, /* WS */
+    { NULL, NULL, NULL, NULL }  /* TLS */
+  },
+  { /* COAP_PROTO_WSS */
+    { NULL, NULL, NULL, NULL }, /* SESSION */
+    { NULL, NULL, NULL, NULL }, /* WS */
+    { NULL, NULL, NULL, NULL }  /* TLS */
+  }
+#endif /* !COAP_WS_SUPPORT */
+#else /* COAP_DISABLE_TCP */
+  { /* COAP_PROTO_TCP */
+    { NULL, NULL, NULL, NULL }, /* SESSION */
+    { NULL, NULL, NULL, NULL }, /* WS */
+    { NULL, NULL, NULL, NULL }  /* TLS */
+  },
+  { /* COAP_PROTO_TLS */
+    { NULL, NULL, NULL, NULL }, /* SESSION */
+    { NULL, NULL, NULL, NULL }, /* WS */
+    { NULL, NULL, NULL, NULL }  /* TLS */
+  },
+  { /* COAP_PROTO_WS */
+    { NULL, NULL, NULL, NULL }, /* SESSION */
+    { NULL, NULL, NULL, NULL }, /* WS */
+    { NULL, NULL, NULL, NULL }  /* TLS */
+  },
+  { /* COAP_PROTO_WSS */
+    { NULL, NULL, NULL, NULL }, /* SESSION */
+    { NULL, NULL, NULL, NULL }, /* WS */
+    { NULL, NULL, NULL, NULL }  /* TLS */
+  }
+#endif /* COAP_DISABLE_TCP */
+};
