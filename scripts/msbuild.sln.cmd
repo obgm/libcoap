8b173135f77c0d82ce7e98d3e36ff912d2598038
Jon Shallow <supjps-libcoap@jpshallow.com>
2023-09-26 16:22:46 +0100

    Windows: Fix workflows OpenSSL install

--- scripts/msbuild.sln.cmd
+++ scripts/msbuild.sln.cmd
@@ -8,6 +8,15 @@ if exist NoTLS rd /s /q NoTLS
 
 cd ..
 
+REM Need to set OPENSSL_INSTALL_PATH so we can build with OpenSSL
+IF EXIST "C:\Program Files\OpenSSL\include\openssl\ssl.h" (
+  SET "OPENSSL_INSTALL_PATH=C:\Program Files\OpenSSL\"
+) ELSE (
+  SET "OPENSSL_INSTALL_PATH=C:\Program Files\OpenSSL-Win64\")
+)
+
+echo OPENSSL_INSTALL_PATH = %OPENSSL_INSTALL_PATH%
+
 rem MSBuild.exe ./win32/libcoap.sln /p:Configuration=NoTLS /p:Platform=x64 /warnaserror
 MSBuild.exe ./win32/libcoap.sln /p:Platform=x64 /warnaserror
 
