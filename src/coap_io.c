obgm/libcoap

commit 224aa74a039caf0297439f6326c1eb3773b75179

coap_io.c: Cleanup timeout checking code in coap_io_process_with_fds()
Should fix CID: 1583564

src/coap_io.c
@@ -1561,7 +1561,8 @@ coap_io_process_with_fds(coap_context_t *ctx, uint32_t timeout_ms,
   timeout = coap_io_prepare_io(ctx, ctx->sockets,
                                (sizeof(ctx->sockets) / sizeof(ctx->sockets[0])),
                                &ctx->num_sockets, before);
-  if (timeout == 0 || timeout_ms < timeout)
+  if (timeout_ms != COAP_IO_NO_WAIT && timeout_ms != COAP_IO_WAIT &&
+      timeout_ms < timeout)
     timeout = timeout_ms;
 
   if (ereadfds) {
@@ -1604,6 +1605,7 @@ coap_io_process_with_fds(coap_context_t *ctx, uint32_t timeout_ms,
     tv.tv_sec = 0;
     timeout = 1;
   } else if (timeout > 0) {
+    /* Ignore timeout_ms == COAP_IO_WAIT as there is something coming up */
     tv.tv_usec = (timeout % 1000) * 1000;
     tv.tv_sec = (long)(timeout / 1000);
   }
@@ -1672,7 +1674,8 @@ coap_io_process_with_fds(coap_context_t *ctx, uint32_t timeout_ms,
 
   timeout = coap_io_prepare_epoll(ctx, before);
 
-  if (timeout == 0 || timeout_ms < timeout)
+  if (timeout_ms != COAP_IO_NO_WAIT && timeout_ms != COAP_IO_WAIT &&
+      timeout_ms < timeout)
     timeout = timeout_ms;
 
   do {
@@ -1681,12 +1684,19 @@ coap_io_process_with_fds(coap_context_t *ctx, uint32_t timeout_ms,
 
     /* Potentially adjust based on what the caller wants */
     if (timeout_ms == COAP_IO_NO_WAIT) {
+      /* Need to return immediately from epoll_wait() */
       etimeout = 0;
-    } else if (timeout == COAP_IO_WAIT) {
-      /* coap_io_prepare_epoll() returned 0 and timeout_ms COAP_IO_WAIT (0) */
+    } else if (timeout == 0 && timeout_ms == COAP_IO_WAIT) {
+      /*
+       * Nothing found in coap_io_prepare_epoll() and COAP_IO_WAIT set,
+       * so wait forever in epoll_wait().
+       */
       etimeout = -1;
     } else if (etimeout < 0) {
-      /* epoll_wait cannot wait longer than this as int timeout parameter */
+      /*
+       * If timeout > INT_MAX, epoll_wait() cannot wait longer than this as
+       * it has int timeout parameter
+       */
       etimeout = INT_MAX;
     }
 

commit e6ffdd65c2948693e53a3194bb95db3ddfde290a

coap_io.c: Further cleanup of timeout checking code
Fixes issue caused by #1314 found during testing.

Code re-worked to make more readable for flow checking.

src/coap_io.c
@@ -1561,9 +1561,6 @@ coap_io_process_with_fds(coap_context_t *ctx, uint32_t timeout_ms,
   timeout = coap_io_prepare_io(ctx, ctx->sockets,
                                (sizeof(ctx->sockets) / sizeof(ctx->sockets[0])),
                                &ctx->num_sockets, before);
-  if (timeout_ms != COAP_IO_NO_WAIT && timeout_ms != COAP_IO_WAIT &&
-      timeout_ms < timeout)
-    timeout = timeout_ms;
 
   if (ereadfds) {
     ctx->readfds = *ereadfds;
@@ -1604,8 +1601,11 @@ coap_io_process_with_fds(coap_context_t *ctx, uint32_t timeout_ms,
     tv.tv_usec = 0;
     tv.tv_sec = 0;
     timeout = 1;
-  } else if (timeout > 0) {
-    /* Ignore timeout_ms == COAP_IO_WAIT as there is something coming up */
+  } else if (timeout == 0 && timeout_ms == COAP_IO_WAIT) {
+    ;
+  } else {
+    if (timeout == 0 || timeout_ms < timeout)
+      timeout = timeout_ms;
     tv.tv_usec = (timeout % 1000) * 1000;
     tv.tv_sec = (long)(timeout / 1000);
   }
@@ -1674,13 +1674,9 @@ coap_io_process_with_fds(coap_context_t *ctx, uint32_t timeout_ms,
 
   timeout = coap_io_prepare_epoll(ctx, before);
 
-  if (timeout_ms != COAP_IO_NO_WAIT && timeout_ms != COAP_IO_WAIT &&
-      timeout_ms < timeout)
-    timeout = timeout_ms;
-
   do {
     struct epoll_event events[COAP_MAX_EPOLL_EVENTS];
-    int etimeout = timeout;
+    int etimeout;
 
     /* Potentially adjust based on what the caller wants */
     if (timeout_ms == COAP_IO_NO_WAIT) {
@@ -1692,12 +1688,17 @@ coap_io_process_with_fds(coap_context_t *ctx, uint32_t timeout_ms,
        * so wait forever in epoll_wait().
        */
       etimeout = -1;
-    } else if (etimeout < 0) {
-      /*
-       * If timeout > INT_MAX, epoll_wait() cannot wait longer than this as
-       * it has int timeout parameter
-       */
-      etimeout = INT_MAX;
+    } else {
+      etimeout = timeout;
+      if (timeout == 0 || timeout_ms < timeout)
+        etimeout = timeout_ms;
+      if (etimeout < 0) {
+        /*
+         * If timeout > INT_MAX, epoll_wait() cannot wait longer than this as
+         * it has int timeout parameter
+         */
+        etimeout = INT_MAX;
+      }
     }
 
     /* Unlock so that other threads can lock/update ctx */