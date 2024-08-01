831135f668c2b14abbed3f7eae982651d8e841af
Jon Shallow <supjps-libcoap@jpshallow.com>
2024-08-01 19:46:41 +0100

    Cleanup building header files for different OS
    'make distclean' removes header files built from the .in files, which
    means that they are not available for different OS builds.
    
    The same is true for CMakeLists.txt.
    
    Remove the need for the .in files and update for libcoap revisions
    by using a new script scripts/fix_version.sh created from
    scripts/fix_version.sh.in.
    
    CI has been updated to check revisions have been updated in the
    appropriate places.
    
    Do not remove libcoap-3.sym and libcoap-3.map when doing 'make distclean'
    as they are git tracked files.

--- scripts/check-missing-updates.sh
+++ scripts/check-missing-updates.sh
@@ -6,15 +6,20 @@
 
 make update-map-file > /dev/null
 
+#
+# Check for any version revision changes
+#
+. scripts/fix_version.sh
+
 git diff > diff_check
 
 WC=`cat diff_check | wc -l`
 if [ $WC != 0 ] ; then
 	echo
-	echo "Please update the master file (usually .in) for the following files"
-	echo "that had the changes reverted by the running of './configure'."
-	echo "There is no need to update the original files, just commit the"
-	echo "changed .in files."
+	echo "Please correct any header files for the appropriate revision"
+	echo "changes. It is possible to do this by running"
+	echo "  . scripts/fix_version.sh"
+	echo "and then commit the changes to the header files."
 	echo
 	echo "Or run 'make update-map-file' and commit the changes to"
 	echo "libcoap-3.map and libcoap-3.sym."
