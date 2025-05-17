7a8dce277ee59c3229f3b92659a3f96b232c1dd1
Jon Shallow <supjps-libcoap@jpshallow.com>
2025-05-17 10:08:40 +0100

    coap_resource.c: Fix observe callback timing window
    If multiple clients subscribe to the same resource, one of the clients
    'goes away', the server will eventually close down the session for that client
    which will then remove any subscriptions, iterating through the different
    resources.
    
    If this happens at the same time that coap_resource_notify_observers() is called
    in another thread, and the GET/FETCH handler is called for a common resource
    and in particular for the session that is now going away, on return from the
    callback the subscription may no longer exist.  This PR fixes this window.
    
    In addition, if the resource subscriber list is being traversed, then a another
    call to coap_notify_observers() for the same resource will not generate a new set
    unsolicited observe responses for multi-thread protection.
    
    Update

--- include/coap3/coap_pdu_internal.h
+++ include/coap3/coap_pdu_internal.h
@@ -413,6 +413,11 @@ coap_pdu_t *coap_new_pdu_lkd(coap_pdu_type_t type, coap_pdu_code_t code,
  */
 void coap_delete_pdu_lkd(coap_pdu_t *pdu);
 
+COAP_STATIC_INLINE void
+coap_pdu_release_lkd(coap_pdu_t *pdu) {
+  coap_delete_pdu_lkd(pdu);
+}
+
 /**
  * Duplicate an existing PDU. Specific options can be ignored and not copied
  * across.  The PDU data payload is not copied across.
