6aef808b1bdf3a073391de50c39bc16cdaf80f49
Jon Shallow <supjps-libcoap@jpshallow.com>
2025-09-12 10:07:41 +0100

    coap_address.c: Validate length of provided host name
    Host names larger than 255 bytes will cause an internal buffer overflow.
    
    Hostnames provided to coap_resolve_address_info() now have their length validated.
    
    Discovered by SecMate (https://secmate.dev).
    
    Sanity check host lengths when parsing a CoAP URI when using the coap_split_uri()
    function.

--- src/coap_uri.c
+++ src/coap_uri.c
@@ -64,6 +64,15 @@ coap_uri_info_t coap_uri_scheme[COAP_URI_SCHEME_LAST] = {
   { "coaps+ws",    443,               0, COAP_URI_SCHEME_COAPS_WS }
 };
 
+/*
+ * Returns  0 All OK
+ *         -1 Insufficient / Invalid parameters
+ *         -2 No '://'
+ *         -3 Ipv6 definition error or no host defined after scheme://
+ *         -4 Invalid port value
+ *         -5 Port defined for Unix domain
+ *         -6 Hostname > 255 chars
+ */
 static int
 coap_split_uri_sub(const uint8_t *str_var,
                    size_t len,
@@ -172,8 +181,10 @@ coap_split_uri_sub(const uint8_t *str_var,
   if (len && *p == '[') {
     /* IPv6 address reference */
     ++p;
+    ++q;
+    --len;
 
-    while (len && *q != ']') {
+    while (len && *q != ']' && (isxdigit(*q) || *q == ':')) {
       ++q;
       --len;
     }
@@ -204,6 +215,12 @@ coap_split_uri_sub(const uint8_t *str_var,
       goto error;
     }
 
+    if ((int)(q - p) > 255) {
+      coap_log_warn("Host name length too long (%d > 255)\n", (int)(q - p));
+      res = -6;
+      goto error;
+    }
+
     COAP_SET_STR(&uri->host, q - p, p);
   }
 
@@ -229,6 +246,7 @@ coap_split_uri_sub(const uint8_t *str_var,
 
       /* check if port number is in allowed range */
       if (uri_port > UINT16_MAX) {
+        coap_log_warn("Port number too big (%ld > 65535)\n", uri_port);
         res = -4;
         goto error;
       }
