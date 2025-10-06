3ac4613866fd6e613a6bfdd93ff8fafaa1c453b5
raspopov <raspopov@cherubicsoft.com>
2025-10-06 21:25:15 +0300

    Switch the CMake TinyDTLS build to the native way
    These changes switch the TinyDTLS library build from Autotools to CMake,
    as support for CMake has now been added. The use of external libraries
    has been optimized. PkgConfig support has been added for library
    detection. The libraries' names were also fixed to use CMake-style
    naming.

--- cmake/FindCUnit.cmake
+++ cmake/FindCUnit.cmake
@@ -1,14 +1,14 @@
-# FindCUnit.cmake
-# -----------------
+# FindCUnit
+# ---------
 #
 # Find the CUnit library.
 #
 # Imported Targets
 # ^^^^^^^^^^^^^^^^
 #
-# This module defines the following `IMPORTED` targets:
+# This module defines the following :prop_tgt:`IMPORTED` targets:
 #
-# ``cunit``
+# ``CUnit::cunit``
 #   The CUnit library, if found.
 #
 # Result Variables
@@ -18,6 +18,8 @@
 #
 # ``CUnit_FOUND``
 #   System has the CUnit library.
+# ``CUnit_VERSION``
+#   The version of CUnit found.
 # ``CUnit_INCLUDE_DIR``
 #   The CUnit include directory.
 # ``CUnit_LIBRARIES``
@@ -28,53 +30,72 @@
 #
 # Set ``CUnit_ROOT_DIR`` to the root directory of a CUnit installation.
 
-find_path(CUnit_INCLUDE_DIR
+if(CUnit_INCLUDE_DIR AND CUnit_LIBRARIES)
+  # in cache already
+  set(CUnit_FIND_QUIETLY TRUE)
+endif()
+
+if(CUnit_ROOT_DIR)
+  set(_CUnit_EXTRA_FIND_ARGS "NO_CMAKE_FIND_ROOT_PATH")
+endif()
+
+find_package(PkgConfig QUIET)
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules(PC_CUNIT QUIET cunit)
+endif()
+
+find_path(
+  CUnit_INCLUDE_DIR
   NAMES CUnit/CUnit.h
   PATH_SUFFIXES include
   HINTS ${PROJECT_SOURCE_DIR}
         ${CMAKE_CURRENT_BINARY_DIR}
         ${CUnit_ROOT_DIR}
-  )
+		${PC_CUNIT_INCLUDE_DIRS}
+        ${_CUnit_EXTRA_FIND_ARGS})
 
-find_library(CUnit_LIBRARIES
+find_library(
+  CUnit_LIBRARIES
   NAMES cunit
   PATH_SUFFIXES lib
   HINTS ${PROJECT_SOURCE_DIR}
         ${CMAKE_CURRENT_BINARY_DIR}
         ${CUnit_ROOT_DIR}
-  )
+		${PC_CUNIT_LIBRARY_DIRS}
+        ${_CUnit_EXTRA_FIND_ARGS})
 
-if(CUnit_LIBRARIES)
-  set(CUnit_FOUND TRUE)
-else()
-  set(CUnit_FOUND FALSE)
-  set(error_message "CUnit not found. Set 'CUnit_ROOT_DIR' to the root directory of a CUnit installation.")
-  if(CUnit_FIND_REQUIRED)
-    message(FATAL_ERROR "${error_message}")
-  elseif(NOT CUDA_FIND_QUIETLY)
-    message(STATUS "${error_message}")
-  endif()
-endif()
-
-if(NOT TARGET cunit)
-  add_library(cunit
-    UNKNOWN
-    IMPORTED
-    )
-  set_target_properties(cunit PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${CUnit_INCLUDE_DIR}"
-    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-    IMPORTED_LOCATION "${CUnit_LIBRARIES}"
-	)
+if(CUnit_INCLUDE_DIR AND EXISTS "${CUnit_INCLUDE_DIR}/CUnit/CUnit.h")
+  file(STRINGS "${CUnit_INCLUDE_DIR}/CUnit/CUnit.h" CUnit_VERSION_STR
+    REGEX "#[\t ]*define[\t ]+CU_VERSION[\t ]+\"[^\"]+\"")
+  string(REGEX REPLACE "^.*CU_VERSION[\t ]+\"([^\"]+)\""
+    "\\1" CUnit_VERSION_STR "${CUnit_VERSION_STR}")
+  set(CUnit_VERSION "${CUnit_VERSION_STR}")
+  unset(CUnit_VERSION_STR)
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CUnit DEFAULT_MSG
-  CUnit_INCLUDE_DIR
-  CUnit_LIBRARIES
-  )
+find_package_handle_standard_args(
+  CUnit
+  FAIL_MESSAGE "Could NOT find CUnit, try to set the path to CUnit root folder in the system variable CUnit_ROOT_DIR"
+  REQUIRED_VARS CUnit_INCLUDE_DIR
+                CUnit_LIBRARIES
+  VERSION_VAR CUnit_VERSION)
+
+if(NOT
+   TARGET
+   CUnit::cunit)
+  add_library(
+    CUnit::cunit
+    UNKNOWN
+    IMPORTED)
+  set_target_properties(
+    CUnit::cunit
+    PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CUnit_INCLUDE_DIR}"
+               VERSION "${CUnit_VERSION}"
+               IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+               IMPORTED_LOCATION "${CUnit_LIBRARIES}")
+endif()
 
 mark_as_advanced(
   CUnit_INCLUDE_DIR
-  CUnit_LIBRARIES
-  )
+  CUnit_LIBRARIES)
--- cmake/Findwolfssl.cmake
+++ cmake/Findwolfssl.cmake
@@ -1,5 +1,5 @@
-# FindWolfSSL.cmake
-# -----------------
+# Findwolfssl
+# -----------
 #
 # Find the wolfSSL library.
 #
@@ -8,7 +8,7 @@
 #
 # This module defines the following :prop_tgt:`IMPORTED` targets:
 #
-# ``wolfssl``
+# ``wolfssl::wolfssl``
 #   The wolfSSL library, if found.
 #
 # Result Variables
@@ -16,8 +16,10 @@
 #
 # This module will set the following variables in your project:
 #
-# ``wolfSSL_FOUND``
+# ``wolfssl_FOUND``
 #   System has the wolfSSL library.
+# ``wolfssl_VERSION``
+#   The version of wolfSSL found.
 # ``WOLFSSL_INCLUDE_DIR``
 #   The wolfSSL include directory.
 # ``WOLFSSL_LIBRARIES``
@@ -28,8 +30,18 @@
 #
 # Set ``WOLFSSL_ROOT_DIR`` to the root directory of a wolfSSL installation.
 
+if(WOLFSSL_INCLUDE_DIR AND WOLFSSL_LIBRARIES)
+  # in cache already
+  set(wolfssl_FIND_QUIETLY TRUE)
+endif()
+
 if(WOLFSSL_ROOT_DIR)
   set(_WOLFSSL_EXTRA_FIND_ARGS "NO_CMAKE_FIND_ROOT_PATH")
+endif()
+
+find_package(PkgConfig QUIET)
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules(PC_WOLFSSL QUIET wolfssl)
 endif()
 
 find_path(
@@ -39,6 +51,7 @@ find_path(
   HINTS ${PROJECT_SOURCE_DIR}
         ${CMAKE_CURRENT_BINARY_DIR}
         ${WOLFSSL_ROOT_DIR}
+		${PC_WOLFSSL_INCLUDE_DIRS}
         ${_WOLFSSL_EXTRA_FIND_ARGS})
 
 find_library(
@@ -48,39 +61,41 @@ find_library(
   HINTS ${PROJECT_SOURCE_DIR}
         ${CMAKE_CURRENT_BINARY_DIR}
         ${WOLFSSL_ROOT_DIR}
+		${PC_WOLFSSL_LIBRARY_DIRS}
         ${_WOLFSSL_EXTRA_FIND_ARGS})
 
-if(WOLFSSL_LIBRARIES)
-  set(wolfSSL_FOUND TRUE)
-else()
-  set(wolfSSL_FOUND FALSE)
-  if(wolfSSL_FIND_REQUIRED)
-    message(FATAL_ERROR "wolfSSL could not be found")
-  endif()
+if(WOLFSSL_INCLUDE_DIR AND EXISTS "${WOLFSSL_INCLUDE_DIR}/wolfssl/version.h")
+  file(STRINGS "${WOLFSSL_INCLUDE_DIR}/wolfssl/version.h" WOLFSSL_VERSION_STR
+    REGEX "#[\t ]*define[\t ]+LIBWOLFSSL_VERSION_STRING[\t ]+\"[^\"]+\"")
+  string(REGEX REPLACE "^.*LIBWOLFSSL_VERSION_STRING[\t ]+\"([^\"]+)\""
+    "\\1" WOLFSSL_VERSION_STR "${WOLFSSL_VERSION_STR}")
+  set(wolfssl_VERSION "${WOLFSSL_VERSION_STR}")
+  unset(WOLFSSL_VERSION_STR)
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-  wolfSSL
-  FOUND_VAR
-  wolfSSL_FOUND
-  REQUIRED_VARS
-  WOLFSSL_INCLUDE_DIR
-  WOLFSSL_LIBRARIES
-  VERSION_VAR)
+  wolfssl
+  FAIL_MESSAGE "Could NOT find WolfSSL, try to set the path to WolfSSL root folder in the system variable WOLFSSL_ROOT_DIR"
+  REQUIRED_VARS WOLFSSL_INCLUDE_DIR
+                WOLFSSL_LIBRARIES
+  VERSION_VAR wolfssl_VERSION)
 
-if(NOT TARGET wolfssl)
+if(NOT
+   TARGET
+   wolfssl::wolfssl)
   add_library(
-    wolfssl
+    wolfssl::wolfssl
     UNKNOWN
     IMPORTED)
   set_target_properties(
-    wolfssl
+    wolfssl::wolfssl
     PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${WOLFSSL_INCLUDE_DIR}"
+               VERSION "${wolfssl_VERSION}"
                IMPORTED_LINK_INTERFACE_LANGUAGES "C"
                IMPORTED_LOCATION "${WOLFSSL_LIBRARIES}")
 endif()
 
-message(STATUS "WOLFSSL_INCLUDE_DIR: ${WOLFSSL_INCLUDE_DIR}")
-message(STATUS "WOLFSSL_LIBRARIES: ${WOLFSSL_LIBRARIES}")
-message(STATUS "WOLFSSL_ROOT_DIR: ${WOLFSSL_ROOT_DIR}")
+mark_as_advanced(
+  WOLFSSL_INCLUDE_DIR
+  WOLFSSL_LIBRARIES)
