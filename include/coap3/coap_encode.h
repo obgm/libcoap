698b897570a8a4d7c837c8831f8c0496ec015170
Tobias Tefke <tobias@tefke.name>
2025-10-15 17:36:25 +0200

    coap_encode.h: Check BSD is defined before evaluating it

--- include/coap3/coap_encode.h
+++ include/coap3/coap_encode.h
@@ -17,7 +17,7 @@
 #ifndef COAP_ENCODE_H_
 #define COAP_ENCODE_H_
 
-#if (BSD >= 199103) || defined(_WIN32)
+#if (defined(BSD) && (BSD >= 199103)) || defined(_WIN32)
 # include <string.h>
 #else
 # include <strings.h>
