53d2a8f95cc6c3c5d94f01b0d095663fc8c8d353
Jon Shallow <supjps-libcoap@jpshallow.com>
2025-12-06 09:50:16 +0000

    multi-threading: Fix no_recursive_check issue

--- include/coap3/coap_threadsafe_internal.h
+++ include/coap3/coap_threadsafe_internal.h
@@ -355,7 +355,6 @@ int coap_lock_lock_func(void);
 #define coap_lock_callback_ret(r,c,func) do { \
     coap_lock_check_locked(c); \
     global_lock.in_callback++; \
-    global_lock.in_callback++; \
     (r) = func; \
     global_lock.in_callback--; \
   } while (0)
